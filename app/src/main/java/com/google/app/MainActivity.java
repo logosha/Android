33f1a2b9537e11c6e6f8246c044ba52051f102e5
logosha <logosha@gmail.com>
2016-09-06 17:05:36 +0300

    photo edit

--- app/src/main/java/com/google/app/MainActivity.java
+++ app/src/main/java/com/google/app/MainActivity.java
@@ -1,12 +1,12 @@
 package com.google.app;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
@@ -26,7 +26,7 @@ public class MainActivity extends Activity {
     TextView secondName;
     TextView phoneNumber;
     boolean isEdit = false;
-
+    final int DIALOG_EXIT = 1;
 
 
     public void onCreate(Bundle savedInstanceState) {
@@ -38,28 +38,28 @@ public class MainActivity extends Activity {
         lvMain.setAdapter(new ArrayAdapter<String>(this, R.layout.my_list_item, items));
 
 
-            lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
-                                        long id) {
-                    TextView textView = (TextView) itemClicked;
-                    String strText = textView.getText().toString();
-                    String[] names = getResources().getStringArray(R.array.names);
+        lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
+                                    long id) {
+                TextView textView = (TextView) itemClicked;
+                String strText = textView.getText().toString();
+                String[] names = getResources().getStringArray(R.array.names);
 
-                    if(strText.equalsIgnoreCase(names[0])) {
-                        startActivity(new Intent(MainActivity.this, ActivitySkills.class));
-                    }
-                    if(strText.equalsIgnoreCase(names[1])) {
-                        startActivity(new Intent(MainActivity.this, ActivityExperience.class));
-                    }
-                    if(strText.equalsIgnoreCase(names[2])) {
-                        startActivity(new Intent(MainActivity.this, ActivityEducation.class));
-                    }
-                    if(strText.equalsIgnoreCase(names[3])) {
-                        startActivity(new Intent(MainActivity.this, ActivityHobbies.class));
-                    }
+                if (strText.equalsIgnoreCase(names[0])) {
+                    startActivity(new Intent(MainActivity.this, ActivitySkills.class));
                 }
-            });
+                if (strText.equalsIgnoreCase(names[1])) {
+                    startActivity(new Intent(MainActivity.this, ActivityExperience.class));
+                }
+                if (strText.equalsIgnoreCase(names[2])) {
+                    startActivity(new Intent(MainActivity.this, ActivityEducation.class));
+                }
+                if (strText.equalsIgnoreCase(names[3])) {
+                    startActivity(new Intent(MainActivity.this, ActivityHobbies.class));
+                }
+            }
+        });
 
 
         settings = (ImageView) findViewById(R.id.settings);
@@ -71,17 +71,17 @@ public class MainActivity extends Activity {
         phoneNumber = (TextView) findViewById(R.id.phone_number);
 
 
-        settings.setOnClickListener(new View.OnClickListener(){
+        settings.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                if (isEdit==false) {
+            public void onClick(View view) {
+                if (isEdit == false) {
                     Toast toast1 = Toast.makeText(MainActivity.this, "включен edit mode", Toast.LENGTH_SHORT);
                     toast1.show();
                     isEdit = true;
                 } else {
                     Toast toast2 = Toast.makeText(MainActivity.this, "выключен edit mode", Toast.LENGTH_SHORT);
                     toast2.show();
-                isEdit = false;
+                    isEdit = false;
                 }
             }
         });
@@ -89,15 +89,29 @@ public class MainActivity extends Activity {
         View.OnClickListener ocl = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(v.getId()==R.id.default_photo)
-                Toast.makeText(MainActivity.this, "это картинка", Toast.LENGTH_LONG).show();
-                else if(v.getId()==R.id.log_out && isEdit==true)
+                if (v.getId() == R.id.default_photo) {
+                    Toast.makeText(MainActivity.this, "это картинка", Toast.LENGTH_LONG).show();
+                } else if (v.getId() == R.id.default_photo && isEdit == true) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Photo content")
+                            .setMessage("Откуда взять фото")
+                            .setCancelable(false)
+                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                    AlertDialog ad = builder.create();
+                    ad.show();
+                } else if (v.getId() == R.id.log_out && isEdit == true) {
                     Toast.makeText(MainActivity.this, "это иконка", Toast.LENGTH_LONG).show();
-                         else if (isEdit==true) {
+                } else if (isEdit == true) {
                     Toast.makeText(MainActivity.this, ((TextView) v).getText(), Toast.LENGTH_LONG).show();
                 }
             }
         };
+
 
         defaultPhoto.setOnClickListener(ocl);
         logOut.setOnClickListener(ocl);
@@ -106,7 +120,6 @@ public class MainActivity extends Activity {
         secondName.setOnClickListener(ocl);
         phoneNumber.setOnClickListener(ocl);
         findViewById(R.id.email).setOnClickListener(ocl);
-
 
 
     }
