c9329b9f794864382993e4c408426802f2eaf008
logosha <logosha@gmail.com>
2016-09-07 22:59:31 +0300

    photo edit

--- app/src/main/java/com/google/app/PhotoChangeActivity.java
+++ app/src/main/java/com/google/app/PhotoChangeActivity.java
@@ -3,55 +3,59 @@ package com.google.app;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.widget.SimpleCursorAdapter;
-import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 /**
  * Created by Алексей on 06.09.2016.
  */
-public class PhotoChangeActivity extends  MyAbstractToolbarActivity {
+public class PhotoChangeActivity extends MyAbstractToolbarActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    GridView gv;
-    ArrayAdapter<String> adapter;
+    GridView gvData;
+    SimpleCursorAdapter scAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_change);
 
-        gv = (GridView) findViewById(R.id.gvPhoto);
+        String[] from = new String[] {MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID};
+        int[] to = new int[] {R.id.gvImage, R.id.tvText};
 
+        gvData = (GridView) findViewById(R.id.gvPhoto);
+        scAdapter = new SimpleCursorAdapter(this, R.layout.grid_item, null, from, to, 0);
+        gvData.setAdapter(scAdapter);
         adjustGridView();
-
-
-        String[] projection = {
-                MediaStore.Images.Thumbnails._ID,
-        };
-
-        Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                null,
-                null,
-                null,
-                null);
-
-        // startManagingCursor(cursor);
-
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
-                android.R.layout.simple_list_item_2,
-                cursor,
-                new String[] {MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID},
-                new int[] {android.R.id.text2,android.R.id.text1});
-
-        gv.setAdapter(adapter);
-
     }
 
     private void adjustGridView() {
-        gv.setNumColumns(GridView.AUTO_FIT);
-        gv.setColumnWidth(80);
-        gv.setVerticalSpacing(5);
-        gv.setHorizontalSpacing(5);
-        gv.setStretchMode(GridView.STRETCH_SPACING_UNIFORM);
+        gvData.setNumColumns(GridView.AUTO_FIT);
+        gvData.setColumnWidth(80);
+        gvData.setVerticalSpacing(5);
+        gvData.setHorizontalSpacing(5);
+        gvData.setStretchMode(GridView.STRETCH_SPACING_UNIFORM);
     }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle bndl) {
+        return new CursorLoader(this,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                new String[]{MediaStore.Images.Media.DATA,
+                        MediaStore.Images.Media._ID},
+                null,
+                null,
+                MediaStore.Images.Media._ID + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        scAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
+
 }
