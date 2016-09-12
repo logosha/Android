baca52f990341fe5bd92a98bf5acf6e2c17bde71
logosha <logosha@gmail.com>
2016-09-12 17:49:05 +0300

    photo edit

--- app/src/main/java/com/google/app/MyCursorAdapter.java
+++ app/src/main/java/com/google/app/MyCursorAdapter.java
@@ -3,13 +3,21 @@ package com.google.app;
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 /**
  * Created by Алексей on 08.09.2016.
@@ -23,16 +31,60 @@ public class MyCursorAdapter extends CursorAdapter {
         cursorInflater = (LayoutInflater) context.getSystemService(
                 Context.LAYOUT_INFLATER_SERVICE);
     }
+    public static final class ViewHolder {
+        ImageView iv;
+    }
 
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         return cursorInflater.inflate(R.layout.grid_item_layout, parent, false);
     }
 
     public void bindView(View view, Context context, Cursor cursor) {
-        TextView textView = (TextView) view.findViewById(R.id.tvText);
-        ImageView imageView = (ImageView) view.findViewById(R.id.gvImage);
-        String title = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media._ID));
-        textView.setText(title);
+        ProgressBar progressBar = null;
+            progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+            progressBar.setVisibility(View.VISIBLE);
 
+
+        ViewHolder holder = new ViewHolder();
+        holder.iv = (ImageView) view.findViewById(R.id.gvImage);
+
+        int imagePathIdx = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        final String imagePath = cursor.getString(imagePathIdx);
+        Picasso.with(context)
+                .load(new File(imagePath))
+                .resize(200, 200)
+                .error(R.drawable.fail)
+                .centerCrop()
+                .into(holder.iv, new ImageLoadedCallback(progressBar) {
+                    @Override
+                    public void onSuccess() {
+                        if (progressBar != null) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                    @Override
+                    public void onError() {
+                        progressBar.setVisibility(View.GONE);
+
+                    }
+                });
+
+    }
+    private class ImageLoadedCallback implements Callback {
+        ProgressBar progressBar;
+
+        public  ImageLoadedCallback(ProgressBar progBar){
+            progressBar = progBar;
+        }
+
+        @Override
+        public void onSuccess() {
+
+        }
+
+        @Override
+        public void onError() {
+
+        }
     }
 }
--- app/src/main/java/com/google/app/PhotoChangeActivity.java
+++ app/src/main/java/com/google/app/PhotoChangeActivity.java
@@ -1,12 +1,18 @@
 package com.google.app;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Алексей on 06.09.2016.
@@ -16,6 +22,7 @@ public class PhotoChangeActivity extends MyAbstractToolbarActivity {
     MyCursorAdapter customAdapter;
     private Cursor mCursor;
     private GridView gridView;
+    final String LOG_TAG = "myLogs";
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +38,24 @@ public class PhotoChangeActivity extends MyAbstractToolbarActivity {
                 null,
                 null,
                 MediaStore.Images.Media._ID + " DESC");
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long i) {
+                Toast.makeText(PhotoChangeActivity.this, "" + view.getId(), Toast.LENGTH_SHORT).show();
+                ImageView newIV = (ImageView) findViewById(R.id.default_photo);
+              Cursor cursor = (Cursor)adapterView.getItemAtPosition(pos);
+              cursor.moveToPosition(pos);
+               String id = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media._ID));
+               Log.d(LOG_TAG, "itemClick: position = " + pos + ", id = " + id);
+                Picasso.with(PhotoChangeActivity.this)
+                        .load()
+                        .resize(200, 200)
+                        .centerInside()
+                        .into(newIV);
+
+            }
+        });
 
 
         new Handler().post(new Runnable() {
