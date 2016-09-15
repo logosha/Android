60e8f0ed148f3b82351882525678bda38d6682e0
logosha <logosha@gmail.com>
2016-09-15 17:42:26 +0300

    photo edit from camera

--- app/src/main/java/com/google/app/ActivityEducation.java
+++ app/src/main/java/com/google/app/ActivityEducation.java
@@ -1,11 +1,18 @@
 package com.google.app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 
-public class ActivityEducation extends MyAbstractToolbarActivity {
+public class ActivityEducation extends MyAbstractToolbarActivity{
+
+    Button btn1;
+    Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +26,27 @@ public class ActivityEducation extends MyAbstractToolbarActivity {
         String htmlAsString = getString(R.string.education_html_text);
         webview.loadData(htmlAsString, "text/html", "UTF-8");
 
+        btn1 = (Button) findViewById(R.id.btnUniverSite);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.univer.kharkov.ua/en"));
+                startActivity(intent);
+            }
+        });
+        btn2 = (Button) findViewById(R.id.btnUniverLocation);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:50.004472, 36.228194"));
+                startActivity(intent);
+            }
+        });
+
     }
+
 
 
 }
