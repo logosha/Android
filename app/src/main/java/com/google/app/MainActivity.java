9b85a720ef934d8ba3bd1be078e36c32ab437b60
logosha <logosha@gmail.com>
2016-09-22 19:04:19 +0300

    final transition to fragments

--- app/src/main/java/com/google/app/MainActivity.java
+++ app/src/main/java/com/google/app/MainActivity.java
@@ -1,11 +1,8 @@
 package com.google.app;
 
-import android.app.Dialog;
+import android.app.Fragment;
 import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -17,41 +14,37 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.google.app.fragments.FragmentEducation;
 import com.google.app.fragments.FragmentExperience;
 import com.google.app.fragments.FragmentHobbies;
+import com.google.app.fragments.FragmentMain;
 import com.google.app.fragments.FragmentSkills;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
-    ImageView settings;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
     ImageView defaultPhoto;
-    ImageView logOut;
-    TextView userID;
-    TextView name;
-    TextView secondName;
-    TextView phoneNumber;
-    boolean isEdit = false;
     final int REQUEST_CODE_CAMERA = 1;
     final int REQUEST_CODE_GALLERY = 2;
+    final String EDU = "edu";
+    final String EXP = "exp";
+    final String SKL = "skl";
+    final String HOB = "hob";
+
+
 
     FragmentEducation fragmentEducation;
     FragmentExperience fragmentExperience;
     FragmentHobbies fragmentHobbies;
     FragmentSkills fragmentSkills;
+    FragmentMain fragmentMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,108 +75,13 @@ public class MainActivity extends AppCompatActivity
         fragmentExperience = new FragmentExperience();
         fragmentHobbies = new FragmentHobbies();
         fragmentSkills = new FragmentSkills();
+        fragmentMain = new FragmentMain();
 
-        findUI();
-        initListeners();
-    }
-
-    private void findUI() {
-        settings = (ImageView) findViewById(R.id.settings);
-        defaultPhoto = (ImageView) findViewById(R.id.default_photo);
-        logOut = (ImageView) findViewById(R.id.log_out);
-        userID = (TextView) findViewById(R.id.user_id);
-        name = (TextView) findViewById(R.id.name);
-        secondName = (TextView) findViewById(R.id.second_name);
-        phoneNumber = (TextView) findViewById(R.id.phone_number);
-    }
-
-
-    private void initListeners() {
-        defaultPhoto.setOnClickListener(this);
-        logOut.setOnClickListener(this);
-        userID.setOnClickListener(this);
-        name.setOnClickListener(this);
-        secondName.setOnClickListener(this);
-        phoneNumber.setOnClickListener(this);
-        findViewById(R.id.email).setOnClickListener(this);
-        settings.setOnClickListener(this);
-    }
-
-    private void showChooserDialog() {
-        final Dialog dialog = new Dialog(MainActivity.this);
-        dialog.setContentView(R.layout.dialog);
-        dialog.setTitle("Photo changing");
-
-        Button btn1 = (Button) dialog.findViewById(R.id.from_galery);
-        btn1.setText(R.string.from_gallery);
-        Button btn2 = (Button) dialog.findViewById(R.id.from_camera);
-        btn2.setText(R.string.from_camera);
-        ImageView image = (ImageView) dialog.findViewById(R.id.imageDialog);
-        image.setImageResource(R.drawable.photo_icon);
-        dialog.show();
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, PhotoChangeActivity.class);
-                dialog.dismiss();
-                startActivityForResult(intent, REQUEST_CODE_GALLERY);
-            }
-        });
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                dialog.dismiss();
-                startActivityForResult(intent, REQUEST_CODE_CAMERA);
-            }
-        });
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            switch (requestCode) {
-                case REQUEST_CODE_GALLERY:
-                    if (data != null && data.hasExtra("path")) {
-                        String path = (String) data.getExtras().get("path");
-                        Picasso.with(MainActivity.this)
-                                .load(new File(path))
-                                .resize(200, 200)
-                                .centerInside()
-                                .into(defaultPhoto);
-                        break;
-                    }
-                case REQUEST_CODE_CAMERA:
-                    if (data != null && data.hasExtra("data")) {
-                        Bitmap thumbnailBitmap = (Bitmap) data.getExtras().get("data");
-                        defaultPhoto.setImageBitmap(thumbnailBitmap);
-                        break;
-                    }
-            }
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.settings && !isEdit) {
-            Toast toast1 = Toast.makeText(MainActivity.this, "включен edit mode", Toast.LENGTH_SHORT);
-            toast1.show();
-            isEdit = true;
-        } else if (v.getId() == R.id.settings && isEdit) {
-            Toast toast2 = Toast.makeText(MainActivity.this, "выключен edit mode", Toast.LENGTH_SHORT);
-            toast2.show();
-            isEdit = false;
-        } else if (v.getId() == R.id.default_photo && isEdit) {
-            showChooserDialog();
-        } else if (v.getId() == R.id.default_photo) {
-            Toast.makeText(MainActivity.this, "это картинка", Toast.LENGTH_LONG).show();
-        } else if (v.getId() == R.id.log_out && isEdit) {
-            Toast.makeText(MainActivity.this, "это иконка", Toast.LENGTH_LONG).show();
-        } else if (isEdit) {
-            Toast.makeText(MainActivity.this, ((TextView) v).getText(), Toast.LENGTH_LONG).show();
-        }
+        String tag = "main";
+        getFragmentManager().beginTransaction().setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_right)
+                .replace(R.id.container, fragmentMain, tag)
+                .addToBackStack(tag)
+                .commit();
     }
 
     @Override
@@ -206,40 +104,31 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        Fragment fragmentToShow = null;
+        String tag = "";
 
         if (id == R.id.nav_education) {
-            String tag = "edu";
-                    fragmentTransaction
-                    .setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_right)
-                    .replace(R.id.container, fragmentEducation, tag)
-                    .addToBackStack(tag);
-
+            fragmentToShow = fragmentEducation;
+            tag = EDU;
         } else if (id == R.id.nav_experience) {
-            String tag = "exp";
-            fragmentTransaction
-                    .setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_right)
-                    .replace(R.id.container, fragmentExperience, tag)
-                    .addToBackStack(tag);
-
+            fragmentToShow = fragmentExperience;
+            tag = EXP;
         } else if (id == R.id.nav_skills) {
-            String tag = "skl";
-            fragmentTransaction
-                    .setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_right)
-                    .replace(R.id.container, fragmentSkills, tag)
-                    .addToBackStack(tag);
-
+            fragmentToShow = fragmentSkills;
+            tag = SKL;
         } else if (id == R.id.nav_hobbies) {
-            String tag = "hob";
-            fragmentTransaction
-                    .setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_right)
-                    .replace(R.id.container, fragmentHobbies, tag)
-                    .addToBackStack(tag);
-
+            fragmentToShow = fragmentHobbies;
+            tag = HOB;
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
-        } fragmentTransaction.commit();
+        }
+                 fragmentTransaction
+                .setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_right)
+                .replace(R.id.container, fragmentToShow, tag)
+                .addToBackStack(tag)
+                .commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
@@ -255,4 +144,5 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
+
 }
---
+++ app/src/main/java/com/google/app/fragments/FragmentMain.java
@@ -0,0 +1,144 @@
+package com.google.app.fragments;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.app.PhotoChangeActivity;
+import com.google.app.R;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+public class FragmentMain extends Fragment implements View.OnClickListener{
+
+    ImageView settings;
+    ImageView defaultPhoto;
+    ImageView logOut;
+    TextView userID;
+    TextView email;
+    TextView name;
+    TextView secondName;
+    TextView phoneNumber;
+    boolean isEdit = false;
+    final int REQUEST_CODE_CAMERA = 1;
+    final int REQUEST_CODE_GALLERY = 2;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.main, container, false);
+
+        settings = (ImageView) v.findViewById(R.id.settings);
+        defaultPhoto = (ImageView) v.findViewById(R.id.default_photo);
+        logOut = (ImageView) v.findViewById(R.id.log_out);
+        userID = (TextView) v.findViewById(R.id.user_id);
+        name = (TextView) v.findViewById(R.id.name);
+        email = (TextView) v.findViewById(R.id.email);
+        secondName = (TextView) v.findViewById(R.id.second_name);
+        phoneNumber = (TextView) v.findViewById(R.id.phone_number);
+
+        defaultPhoto.setOnClickListener(this);
+        logOut.setOnClickListener(this);
+        userID.setOnClickListener(this);
+        name.setOnClickListener(this);
+        secondName.setOnClickListener(this);
+        phoneNumber.setOnClickListener(this);
+        email.setOnClickListener(this);
+        settings.setOnClickListener(this);
+
+
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.settings && !isEdit) {
+            Toast toast1 = Toast.makeText(getActivity(), "включен edit mode", Toast.LENGTH_SHORT);
+            toast1.show();
+            isEdit = true;
+        } else if (v.getId() == R.id.settings && isEdit) {
+            Toast toast2 = Toast.makeText(getActivity(), "выключен edit mode", Toast.LENGTH_SHORT);
+            toast2.show();
+            isEdit = false;
+        } else if (v.getId() == R.id.default_photo && isEdit) {
+            showChooserDialog();
+        } else if (v.getId() == R.id.default_photo) {
+            Toast.makeText(getActivity(), "это картинка", Toast.LENGTH_LONG).show();
+        } else if (v.getId() == R.id.log_out && isEdit) {
+            Toast.makeText(getActivity(), "это иконка", Toast.LENGTH_LONG).show();
+        } else if (isEdit) {
+            Toast.makeText(getActivity(), ((TextView) v).getText(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void showChooserDialog() {
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.dialog);
+        dialog.setTitle("Photo changing");
+
+        Button btn1 = (Button) dialog.findViewById(R.id.from_galery);
+        btn1.setText(R.string.from_gallery);
+        Button btn2 = (Button) dialog.findViewById(R.id.from_camera);
+        btn2.setText(R.string.from_camera);
+        ImageView image = (ImageView) dialog.findViewById(R.id.imageDialog);
+        image.setImageResource(R.drawable.photo_icon);
+        dialog.show();
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), PhotoChangeActivity.class);
+                dialog.dismiss();
+                startActivityForResult(intent, REQUEST_CODE_GALLERY);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                dialog.dismiss();
+                startActivityForResult(intent, REQUEST_CODE_CAMERA);
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_CODE_GALLERY:
+                    if (data != null && data.hasExtra("path")) {
+                        String path = (String) data.getExtras().get("path");
+                        Picasso.with(getActivity())
+                                .load(new File(path))
+                                .resize(200, 200)
+                                .centerInside()
+                                .into(defaultPhoto);
+                        break;
+                    }
+                case REQUEST_CODE_CAMERA:
+                    if (data != null && data.hasExtra("data")) {
+                        Bitmap thumbnailBitmap = (Bitmap) data.getExtras().get("data");
+                        defaultPhoto.setImageBitmap(thumbnailBitmap);
+                        break;
+                    }
+            }
+        }
+    }
+
+
+}
