742533b53c9cedd985fbe139f2fc8818b47bd438
logosha <logosha@gmail.com>
2016-09-26 13:52:18 +0300

    download project to SD with ThinDownloadManager 1.2.5

--- app/src/main/java/com/google/app/fragments/FragmentAbout.java
+++ app/src/main/java/com/google/app/fragments/FragmentAbout.java
@@ -1,15 +1,12 @@
 package com.google.app.fragments;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.os.Environment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,21 +14,19 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.app.R;
+import com.thin.downloadmanager.DefaultRetryPolicy;
 import com.thin.downloadmanager.DownloadRequest;
 import com.thin.downloadmanager.DownloadStatusListener;
 import com.thin.downloadmanager.ThinDownloadManager;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class FragmentAbout extends Fragment {
     Button btnCheck;
     Button btnDownload;
+    ThinDownloadManager downloadManager;
     Uri downloadUri = Uri.parse("https://github.com/logosha/android/archive/master.zip");
-    Uri destinationUri = Uri.parse( Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/android.zip");
+    Uri destinationUri = Uri.parse(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/android.zip");
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -61,26 +56,29 @@ public class FragmentAbout extends Fragment {
 }
 
     private void downloadProject() {
-        ThinDownloadManager downloadManager = new ThinDownloadManager(5);
-        DownloadRequest downloadRequest = new DownloadRequest(downloadUri).setDestinationURI(destinationUri);
+        downloadManager = new ThinDownloadManager();
+                DownloadRequest downloadRequest = new DownloadRequest(downloadUri)
+                .addCustomHeader("Auth-Token", "YourTokenApiKey")
+                .setRetryPolicy(new DefaultRetryPolicy())
+                .setDestinationURI(destinationUri)
+                .setPriority(DownloadRequest.Priority.HIGH)
+                .setDownloadListener(new DownloadStatusListener() {
+                                        @Override
+                                        public void onDownloadComplete(int id) {
+                                            Toast.makeText(getActivity(), "Загрузка завершена", Toast.LENGTH_LONG).show();
+                                        }
+
+                                        @Override
+                                        public void onDownloadFailed(int id, int errorCode, String errorMessage) {
+
+                                        }
+
+                                        @Override
+                                        public void onProgress(int id, long totalBytes, long downloadedBytes, int progress) {
+
+                                        }
+                                    });
         downloadManager.add(downloadRequest);
-
-        DownloadStatusListener downloadListener = new DownloadStatusListener() {
-            @Override
-            public void onDownloadComplete(int id) {
-                Toast.makeText(getActivity(), "Загрузка завершена", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onDownloadFailed(int id, int errorCode, String errorMessage) {
-            }
-
-            @Override
-            public void onProgress(int id, long totalBytes, int progress) {
-
-            }
-        };
-        downloadRequest.setDownloadListener(downloadListener);
     }
 
 
